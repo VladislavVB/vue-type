33861d7bb9617d0223415c53781b37fa8ebf5ff0
Vlad Vlad <burdukovskij.vlad@mail.ru>
2022-11-11 00:41:11 +0300

    fff

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 import Job from './types/Job'
 
