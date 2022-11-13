c31dce4e07091d0fee8f2b75ce37a16b0d2167d3
Vlad Vlad <burdukovskij.vlad@mail.ru>
2022-11-13 13:27:23 +0300

    add pinia & axios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import router from './router/index'
 import App from './App.vue'
 import './assets/css/main.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+
+</script>
+<template>
+  <div class="page register">
+    <div class="form">
+      <div class="input-group">
+        <input placeholder="Username" type="text" >
+      </div>
+    </div>
+  </div>
+</template>
